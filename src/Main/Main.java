d98fd98733790ca675c0c791cb9d73784f9c0ce5
kazuaki tamaki <e185704@ie.u-ryukyu.ac.jp>
2018-11-26 15:07:16 +0900

    step2 commit

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,6 +1,17 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args){
         String str = null;
-        System.out.println(str.length());
+//        System.out.println(str.length());
+        try{
+            System.out.println(str.length());
+        }
+        catch (NullPointerException e){
+            System.out.println("NullPointExeptionエラー");
+            System.out.println(e.getMessage());
+        }
+
     }
 }
