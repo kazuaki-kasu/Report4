55c9370d7ee0bcbf328a0b029d8bb290213e971a
kazuaki tamaki <e185704@ie.u-ryukyu.ac.jp>
2018-11-26 19:58:34 +0900

    step3 commit

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,16 +2,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args){
-        String str = null;
-//        System.out.println(str.length());
-        try{
-            System.out.println(str.length());
-        }
-        catch (NullPointerException e){
-            System.out.println("NullPointExeptionエラー");
-            System.out.println(e.getMessage());
-        }
-
+    public static void main(String[] args) {
+        String str = "百二十三";
+        int value;
+        value = Integer.parseInt(str);
     }
 }
